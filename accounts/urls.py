a8a62ca2692dc93ceb1ce91f38ba3fb053a19b8e
Namitha0203 <namithaprdp03@gmail.com>
2025-09-23 03:13:55 +0400

    Cleaned up URL patterns and added JWT token route

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, CustomLoginView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),  # 👈 Add this line
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
+
 
